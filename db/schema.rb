b12ee9c6a789846ef3a2a2dee2adcf64bc102644
Tessa Davis <tessadavis06@gmail.com>
2014-03-03 19:22:36 +1100

    associated a user id with a new contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140303053917) do
+ActiveRecord::Schema.define(version: 20140303081932) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20140303053917) do
     t.text     "info"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "contacts", ["user_id"], name: "index_contacts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
