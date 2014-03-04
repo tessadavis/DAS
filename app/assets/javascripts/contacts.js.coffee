4adae80773952e9ed4a40050699373fe18ccfc56
Tessa Davis <tessadavis06@gmail.com>
2014-03-04 15:40:34 +1100

    so much done - styling of forms, relationship with userid and contact

--- app/assets/javascripts/contacts.js.coffee
+++ app/assets/javascripts/contacts.js.coffee
@@ -2,7 +2,10 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$ ->
+ready = ->
 	$('.contact').hover (event) -> 
 		console.log("Hover triggered")
 		$(this).toggleClass("hover")
+
+$(document).ready(ready)
+$(document).on('page:load', ready)
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -69,6 +69,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :category, :area, :phone, :website, :email, :info)
+      params.require(:contact).permit(:name, :category, :area, :phone, :website, :email, :info, :user_id)
     end
 end
