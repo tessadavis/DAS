4bb7483bbe053beb3b3f222d60f692ac3172d167
Tessa Davis <tessadavis06@gmail.com>
2014-03-05 20:12:18 +1100

    secret key ofr devise added

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.secret_key = 'some1234keyq23' if Rails.env == 'production'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
