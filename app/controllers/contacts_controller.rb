2492fcac11607a334cb40cbebc6187872b975f0c
Tessa Davis <tessadavis06@gmail.com>
2014-03-05 15:35:49 +1100

    added custom routes integration test and made sure you have to be logged in to create a new contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new]
 
   # GET /contacts
   # GET /contacts.json
--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -11,7 +11,14 @@ class ContactsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:contacts)
   end
 
-  test "should get new" do
+  test "should be redirected when not logged in" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render the new page when logged in" do
+    sign_in users(:tessa)
     get :new
     assert_response :success
   end
